0ec9cf38c8440f2039011112ebc67e37d9263113
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-12 18:09:51 +0100

    introduced critical section to configure controllers

--- ryu-rest/app.py
+++ ryu-rest/app.py
@@ -6,16 +6,15 @@ from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.ofproto import ofproto_v1_3
 # DON'T REMOVE THIS IMPORT
 from ryu.topology.api import get_switch, get_link, get_host
-from ryu.lib import hub
-from http import server
 from l2_controller import L2Controller
 from l3_controller import L3Controller
 from topology_controller import TopologyController
 from ofctl import add_flow
 from notifications import HTTPClient, NotificationsController
+from threading import Semaphore
 
-NETCONF_BACKEND_URL = 'http://net-conf:4000/'
-IP_ADDRESSES_ENDPOINT = NETCONF_BACKEND_URL + 'allDataPathsIps'
+NETCONF_BACKEND_URL = 'http://127.0.0.1:4000/'
+IP_ADDRESSES_ENDPOINT = NETCONF_BACKEND_URL + 'allDataPathIps'
 IP_TABLES_ENDPOINT = NETCONF_BACKEND_URL + 'allIpTables'
 
 class App(app_manager.RyuApp):
@@ -29,7 +28,9 @@ class App(app_manager.RyuApp):
         # When the ryu application starts, the controller
         # waits for L3 configuration from the network configuration service.
         self.configured = False
-        self.waiting_l3_configuration = True
+        self.first_thread = True
+        self.finished_configuration = False
+        self.config_semaphore = Semaphore(1)
 
         # A data structure to keep track of switches that are
         # waiting to be configured
@@ -49,9 +50,10 @@ class App(app_manager.RyuApp):
     def _state_change_handler(self, ev):
         # For the time being, the controller does not handle the
         # situation where a new switch is added to the physical network
-        if self.configured:
+        if self.configured and self.finished_configuration:
             raise Exception(f"A new switch entered the network, dpid: {ev.datapath.id}")
         else:
+            print(f"Datapath {ev.datapath.id} needs configuration")
             self.not_configured_datapaths[ev.datapath.id] = ev.datapath
 
     
@@ -78,28 +80,9 @@ class App(app_manager.RyuApp):
         # while the network has yet to be configured at layer 3,
         # all packets are dropped
         if self.configured:
-
-            # if the l3 configuration is now available, retrieve it
-            # from the network configuration service
-            if self.waiting_l3_configuration:
-                self.waiting_l3_configuration = False
-                ip_addresses = HTTPClient.get(IP_ADDRESSES_ENDPOINT)
-                ip_tables = HTTPClient.get(IP_TABLES_ENDPOINT)
-
-                for (dpid, datapath) in ip_addresses.items():
-                    # The switch is registered as a L3 switch
-                    dp_ips = ip_addresses[dpid]
-                    dp_ip_table = ip_tables[dpid]
-                    self.l3_controller.register_datapath(datapath, dp_ips, dp_ip_table)
-                    del self.not_configured_datapaths[dpid]
-                
-                # switches that did not get ip addresses assigned are
-                # configured as L3 switches
-                for (dpid, datapath) in self.not_configured_datapaths.items():
-                    self.l2_controller.register_datapath(datapath)
-                    del self.not_configured_datapaths[dpid]
-
-            else:
+            # If the configuration has been applyed, there is no need to bother
+            # with trying to enter the critical section
+            if self.finished_configuration:
 
                 if ev.msg.datapath.id in self.l2_controller.datapaths:
                     self.l2_controller.packet_in_handler(ev.msg)
@@ -107,3 +90,38 @@ class App(app_manager.RyuApp):
                     self.l3_controller.packet_in_handler(ev.msg)
                 else:
                     raise Exception(f"Unknown datapath {ev.msg.datapath.id}\n")
+            
+            # Otherwise if the l3 configuration is now available, retrieve it
+            # from the network configuration service. 
+            else:
+                # The first event handling thread that is able to acquire the
+                # semaphore configures the controller. All other threads will
+                # walk past the 'if' statement
+                self.config_semaphore.acquire()
+                if self.first_thread:
+                    ip_addresses = HTTPClient.get(IP_ADDRESSES_ENDPOINT, None)
+                    ip_tables = HTTPClient.get(IP_TABLES_ENDPOINT, None)
+
+                    for dpid in ip_addresses:
+                        # The switch is registered as a L3 switch
+                        dp_ips = ip_addresses[dpid]
+                        dp_ip_table = ip_tables[dpid]
+                        datapath = self.not_configured_datapaths[int(dpid)]
+                        self.l3_controller.register_datapath(datapath, dp_ips, dp_ip_table)
+                        del self.not_configured_datapaths[int(dpid)]
+                    
+                    # switches that did not get ip addresses assigned are
+                    # configured as L3 switches
+                    for dpid in self.not_configured_datapaths:
+                        datapath = self.not_configured_datapaths[dpid]
+                        self.l2_controller.register_datapath(datapath)
+
+                    # Trigger garbage collection, as this data structure will not
+                    # be used anymore
+                    self.not_configured_datapaths = None
+
+                    self.first_thread = False
+                    self.finished_configuration = True
+
+                    # Release the semaphore to unblock other threads
+                    self.config_semaphore.release()
